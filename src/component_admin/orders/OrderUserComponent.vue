09b6d3357d057add698a7a0a0eb023049441ec81
Dillon Gunn <dillongunn@gmail.com>
2022-11-15 16:16:40 +0800

    live changes

--- src/component_admin/orders/OrderUserComponent.vue
+++ src/component_admin/orders/OrderUserComponent.vue
@@ -1,49 +1,133 @@
 <template>
     <v-container fill-height fluid class="pa-0 ma-0">
-        <v-data-table
-            :headers="orders_header"
-            :items="orders"
-            style="width:100%"
-            class="elevation-1"
+        <v-tabs
+          v-model="tab"
+          grow
+          dark
+          color="basil"
+          background-color="deep-purple accent-4"
         >
-            <template v-slot:item="{item}">
-                <tr>
+          <v-tabs-slider color="yellow"></v-tabs-slider>
+
+          <v-tab
+            v-for="service in services"
+            :key="service.id"
+            :href="`#tab-${service.id}`"
+          >
+            <label
+                style="color: white;"
+            >
+                {{ service.name }}
+            </label>
+          </v-tab>
+        </v-tabs>
+        <v-tabs-items 
+            v-model="tab"
+            class="mt-5 justify-center align-center"
+            style="width: 100%;"
+        >
+            <v-tab-item
+                value="tab-1"
+            >
+              <v-data-table :headers="orders_header" :items="orders" style="width:100%" class="elevation-1">
+                <template v-slot:item="{item}">
+                  <tr>
                     <td>
-                        {{item.order_type}}
+                      {{item.order_type}}
                     </td>
                     <td>
-                        {{item.name}}
+                      {{item.name}}
                     </td>
                     <td>
-                        {{moment(item.created_at).format("h:mm:ss a, MMM DD, YYYY")}}
+                      {{moment(item.created_at).format("h:mm:ss a, MMM DD, YYYY")}}
                     </td>
                     <td>
-                        ₱{{item.price}}
+                      ₱{{item.price}}
                     </td>
                     <td v-if="item.paid">
-                        Yes
+                      Yes
                     </td>
                     <td v-else>
-                        No
+                      No
                     </td>
                     <td>
-                        <v-btn
-                            dark
-                            v-if="!item.paid"
-                            @click="paymongo(item)"
-                        >
+                      <v-btn dark v-if="!item.paid" @click="order_type = 'food'; paymongo(item)">
+                        Pay
+                      </v-btn>
+                      <v-btn disabled v-else>
+                        Paid
+                      </v-btn>
+                    </td>
+                  </tr>
+                </template>
+              </v-data-table>
+            </v-tab-item>
+            <v-tab-item
+                value="tab-2"
+            >
+              <v-data-table :headers="orders_header_transpo" :items="orders_transpo" style="width:100%" class="elevation-1">
+                <template v-slot:item="{item}">
+                  <tr>
+                    <td>
+                      {{ item.pick_up_location }}
+                    </td>
+                    <td>
+                      {{ item.drop_off_location }}
+                    </td>
+                    <td>
+                      {{ moment(item.pick_up_date).format("h:mm:ss a, MMM DD, YYYY")}}
+                    </td>
+                    <td>
+                      {{ item.transpo_type }}
+                    </td>
+                    <td v-if="item.payable == 0">
+                        Waiting for quotation
+                    </td>
+                    <td v-else>
+                        ₱{{ item.payable }}
+                    </td>
+                    <td v-if="item.payable > 0">
+                        <v-btn dark v-if="!item.is_paid"
+                            @click="order_type = 'transpo'; paymongo(item)">
                             Pay
                         </v-btn>
-                        <v-btn
-                            disabled
-                            v-else
-                        >
+                        <v-btn disabled v-else>
                             Paid
                         </v-btn>
                     </td>
-                </tr>
-            </template>
-        </v-data-table>
+                    <td v-else></td>
+                  </tr>
+                </template>
+              </v-data-table>
+            </v-tab-item>
+            <v-tab-item
+                value="tab-3"
+            >
+              <v-data-table :headers="orders_header_massage" :items="orders_massage" style="width:100%" class="elevation-1">
+                <template v-slot:item="{item}">
+                  <tr>
+                    <td>
+                      {{item.name}}
+                    </td>
+                    <td>
+                      {{ moment(item.massage_date).format("h:mm:ss a, MMM DD, YYYY") }}
+                    </td>
+                    <td>
+                      ₱{{item.payable}}
+                    </td>
+                    <td>
+                      <v-btn dark v-if="!item.paid" @click="order_type = 'massage'; paymongo(item)">
+                        Pay
+                      </v-btn>
+                      <v-btn disabled v-else>
+                        Paid
+                      </v-btn>
+                    </td>
+                  </tr>
+                </template>
+              </v-data-table>
+            </v-tab-item>
+        </v-tabs-items>
         
       <v-row justify="center">
         <v-dialog
@@ -196,6 +280,21 @@ export default {
   props: [
   ],
   data: () => ({
+    services: [
+      {
+        id: 1,
+        name: 'Food'
+      },
+      {
+        id: 2,
+        name: 'Transportation'
+      },
+      {
+        id: 3,
+        name: 'Spa & Massage'
+      }
+    ],
+    tab: null,
     id: null,
     paymongo_model: false,
     amount: 0,
@@ -236,10 +335,59 @@ export default {
             sortable: false
         }
     ],
-    orders: []
+    orders_header_transpo: [
+      {
+        text: 'Pick up',
+        sortable: false
+      },
+      {
+        text: 'Drop off',
+        sortable: false
+      },
+      {
+        text: 'Time',
+        sortable: false
+      },
+      {
+        text: 'Vehicle',
+        sortable: false
+      },
+      {
+        text: 'Price',
+        sortable: false
+      },
+      {
+        text: '',
+        sortable: false
+      }
+    ],
+    orders_header_massage: [
+      {
+        text: 'Massage',
+        sortable: false
+      },
+      {
+        text: 'Schedule',
+        sortable: false
+      },
+      {
+        text: 'Price',
+        sortable: false
+      },
+      {
+        text: 'Pay',
+        sortable: false
+      }
+    ],
+    orders: [],
+    orders_transpo: [],
+    orders_massage: [],
+    order_type: null
   }),
   async mounted () {
     this.get_orders()
+    this.get_orders_transpo()
+    this.get_orders_massage()
   },
   created () {
   },
@@ -256,10 +404,30 @@ export default {
             console.log(this.orders)
         })
     },
+    async get_orders_transpo() {
+      await this.$axios.get('/user/auth_user/get_orders_user_transpo')
+        .then(({ data }) => {
+          this.orders_transpo = data
+          console.log(this.orders_transpo)
+        })
+    },
+    async get_orders_massage() {
+      await this.$axios.get('/user/auth_user/get_orders_user_massage')
+        .then(({ data }) => {
+          this.orders_massage = data
+          console.log(this.orders_massage)
+        })
+    },
     paymongo(data){
       this.paymongo_model = true
-      this.amount = data.price
+      if (data.price != null) {
+        this.amount = data.price
+      }
+      if (data.payable != null) {
+        this.amount = data.payable
+      }
       this.id = data.id
+      console.log(this.order_type)
     },
     async paymongo_card_create_intent() { // PAY WITH CARD, CREATE PAYMENT
       this.paymongo_model = false
@@ -280,11 +448,21 @@ export default {
         exp_year: this.details.exp_year,
         cvc: this.details.cvc,
         client_key: this.intent_id,
-        amount: this.amount
+        amount: this.amount,
+        order_type: this.order_type
       }
       await this.$axios.post('r/payment/paymongo_card_create_method_food', payload)
       .then(({data}) => {
-        this.orders = data
+        //this.orders = data
+        if (this.order_type == 'food') {
+          this.orders = data
+        }
+        if (this.order_type == 'transpo') {
+          this.orders_transpo = data
+        }
+        if (this.order_type == 'massage') {
+          this.orders_massage = data
+        }
         this.card_details_model = false
       })
     },
